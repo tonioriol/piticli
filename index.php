8b44cc6053fcc0ae1e33e5bb5c8c7b60f6db4cc3
Toni Oriol <tonioriol@gmail.com>
2018-05-07 22:04:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	<script>
 		document.addEventListener('DOMContentLoaded', function(e) {
-			document.querySelector('#sentence').addEventListener('keyup', function(e) {
-				document.querySelector('#result').innerHTML = e.target.value.replace(/a|e|i|o|u/g, 'i')
-			})
-		})
+      document.querySelector('#sentence').addEventListener('keyup', piticli)
+      piticli()
+    })
+    function piticli () {
+      document.querySelector('#result').innerHTML = document.querySelector('#sentence').value.replace(/a|e|i|o|u/g, 'i')
+    }
 	</script>
 </head>
 <body>
@@ -18,7 +20,7 @@
         <header>
           <h1>PITICLI</h1>
         </header>
-        <textarea id="sentence"></textarea>
+        <textarea id="sentence"><?= urldecode($_GET['s']) ?></textarea>
         <div id="result"></div>
       </div>
     </div>
